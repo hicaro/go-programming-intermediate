1707374a667e1b80986ff99f90b458e43ede8e20
Hicaro Adriano <hicaro.adriano@beckshybrids.com>
2019-06-14 14:48:54 -0700

    Ninja level 8

---
+++ exercises/level 8/exercise_3.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func exercise_3() {
+	u1 := agent{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := agent{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := agent{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	agents := []agent{u1, u2, u3}
+
+	// your code goes here
+	json.NewEncoder(os.Stdout).Encode(agents)
+}
