f8a411293c12f51a27c7ca886f3532db1717cda8
Hicaro Adriano <hicaro.adriano@beckshybrids.com>
2019-06-28 07:30:44 -0700

    Doc information

---
+++ section-26: documentation/main.go
@@ -0,0 +1,13 @@
+package main
+
+// go doc: prints the documentation for a package, const, func, type, var, or method
+
+// godoc: extracts and generates documentation for Go programs.
+// contains `-src` flag to show source code e.g.: `godoc -src erros New`
+// constains `-http` flag to spawn up a server to server a doc web page
+
+// godoc.org
+
+func main() {
+
+}
